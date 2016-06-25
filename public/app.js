13e305eead31161795c669a8a55012d84d5e2352
isaacg11 <isaacgreywebdeveloper@gmail.com>
2016-06-24 20:49:01 -0700

    initial commit

---
+++ public/app.js
@@ -0,0 +1,18 @@
+(function() {
+  'use strict';
+  var app = angular.module('master', ['ui.router','ngStamplay'])
+  .config(Config);
+  Config.$inject = ['$stateProvider', '$urlRouterProvider','$locationProvider'];
+
+
+function Config($stateProvider, $urlRouterProvider, $locationProvider) {
+  $stateProvider.state('Home',{
+    url: '/',
+    templateUrl: './public/views/home.html'
+  });
+  
+  $urlRouterProvider.otherwise('/');
+  //$locationProvider.html5Mode(true);
+  }
+
+})();
---
+++ public/controllers/homeCtrl.js
@@ -0,0 +1,25 @@
+//CONTROLLER
+(function() {
+  'use strict';
+  angular.module('master')
+  .controller('homeCtrl', homeCtrl);
+  homeCtrl.$inject = [
+    '$state',
+    "$http",
+    "$scope", 
+    "$stamplay",
+    '$window'
+  ];
+  function homeCtrl(
+    $state, 
+    $http, 
+    $scope, 
+    $stamplay, 
+    $window
+    )
+  {
+
+    $scope.hello = "hello world";
+
+  }
+  })();
