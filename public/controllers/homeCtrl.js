e852e823023defaacdc614ee84eeab5d9aef3510
isaacg11 <isaacgreywebdeveloper@gmail.com>
2016-06-25 21:02:24 -0700

    github repos section completed

--- public/controllers/homeCtrl.js
+++ public/controllers/homeCtrl.js
@@ -4,6 +4,7 @@
   angular.module('master')
   .controller('homeCtrl', homeCtrl);
   homeCtrl.$inject = [
+    "General",
     '$state',
     "$http",
     "$scope", 
@@ -11,6 +12,7 @@
     '$window'
   ];
   function homeCtrl(
+    General,
     $state, 
     $http, 
     $scope, 
@@ -19,9 +21,58 @@
     )
   {
 
-    $scope.test = function() {
-      alert('blah!');
-    }
+    //on page load, get and display github repo data
+
+    var arr = [];
+
+    General.getRepo1().then(function(res){
+      console.log(res);
+      var repo_one = {
+        name: res.data.name,
+        forks: res.data.forks_count,
+        stars: res.data.stargazers_count,
+        watchers: res.data.watchers,
+        url: res.data.homepage,
+        updated: res.data.updated_at,
+        description: "Recipe lookup website"
+      };
+
+      arr.push(repo_one);
+
+      General.getRepo2().then(function(res){
+        var repo_two = {
+          name: res.data.name,
+          forks: res.data.forks_count,
+          stars: res.data.stargazers_count,
+          watchers: res.data.watchers,
+          url: res.data.homepage,
+          updated: res.data.updated_at,
+          description: "Food locator website"
+        };
+
+        arr.push(repo_two);
+
+        General.getRepo3().then(function(res){
+          var repo_three = {
+            name: res.data.name,
+            forks: res.data.forks_count,
+            stars: res.data.stargazers_count,
+            watchers: res.data.watchers,
+            url: res.data.homepage,
+            updated: res.data.updated_at,
+            description: "Photo album app"
+          };
+
+          arr.push(repo_three);
+          //console.log(arr);
+          $scope.repos = arr;
+        });
+      })
+    });
+
+    $scope.goToRepo = function(url) {
+      window.location = url;
+    };
 
   }
   })();
---
+++ public/services/General.js
@@ -0,0 +1,34 @@
+
+//FACTORY
+
+(function() {
+  'use strict';
+  angular.module('master')
+  .factory('General', General);
+
+  function General($http, $q){
+	return {
+    	getRepo1 : function(){
+        var q = $q.defer();
+    		$http.get('https://api.github.com/repos/isaacg11/rapid-recipe').then(function(res) {
+          q.resolve(res);
+        });
+        return q.promise;
+    	},
+      getRepo2 : function(){
+        var q = $q.defer();
+        $http.get('https://api.github.com/repos/isaacg11/food-findr').then(function(res) {
+          q.resolve(res);
+        });
+        return q.promise;
+      },
+      getRepo3 : function(){
+        var q = $q.defer();
+        $http.get('https://api.github.com/repos/N3phi1im/Whereabout').then(function(res) {
+          q.resolve(res);
+        });
+        return q.promise;
+      }
+	};
+}
+})();
